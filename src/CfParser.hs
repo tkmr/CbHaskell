80ddc72fae80896f80e85b9a1061f990c604e753
tkmr <tatsuya@tkmr.org>
2009-09-13 02:02:15 +0900

    binary files added to .gitignore

--- src/CfParser.hs
+++ src/CfParser.hs
@@ -5,12 +5,12 @@ import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import qualified Text.ParserCombinators.Parsec.Token as P
 
---quickcheck
-import Test.QuickCheck
-import System.Random
-import Control.Monad
-------------
+runParse parser []     = []
+runParse parser (x:xs) = case (parse parser "" x) of
+                           Left error -> runParse parser xs
+                           Right datas -> datas:(runParse parser xs)
 
+---------------------------------                                          
 lexer :: P.TokenParser ()
 lexer = P.makeTokenParser (javaStyle {
                              reservedNames   = ["return", "total"]
@@ -20,9 +20,6 @@ lexer = P.makeTokenParser (javaStyle {
 reservedOp = P.reservedOp lexer
 reserved = P.reserved lexer
 lexeme = P.lexeme lexer
-
----------------------------------
-
 whiteSpace = P.whiteSpace lexer
 identifier = P.identifier lexer
 semi = lexeme(char ';')
--- src/Main.hs
+++ src/Main.hs
@@ -4,23 +4,3 @@ import Text.ParserCombinators.Parsec
 import Test.QuickCheck
 
 main = do putStr "hello"
-    
-run :: Show a => Parser a -> String -> IO ()
-run p input = case (parse p "" input) of
-                Left err -> do { putStr "parse error at "
-                               ; print err
-                               }
-                Right x  -> print x
-
-runLex :: Show a => Parser a -> String -> IO ()
-runLex p input = run (do { whiteSpace
-                         ; x <- p
-                         ; eof
-                         ; return x
-                           }) input
-
-prop_assign xs = isString $ runLex assign "hoge = age"
-    where
-      types = xs::[Char]
-
-isString io = True
